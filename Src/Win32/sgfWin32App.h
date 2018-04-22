89a41ea5a82044dd76e3e3166a39835001b6d2af
xxl534 <xiexuelong534@163.com>
2018-04-22 10:12:29 +0800

    core engine win32app

---
+++ Src/Win32/sgfWin32App.h
@@ -0,0 +1,20 @@
+#pragma once
+/*************************************
+	created: 2018/04/22 09:46 
+	filename:	G:\Visual Studio 2015\SimpleGameFrame\Src\Win32\sgfWin32App.h
+	file path:	G:\Visual Studio 2015\SimpleGameFrame\Src\Win32
+	file base:	sgfWin32App
+	file ext:	h	
+	author: 	xiexuelong
+	
+	purpose:
+*************************************/
+
+namespace sgf
+{
+	class Win32App
+	{
+	public:
+		Win32App();
+	};
+}
